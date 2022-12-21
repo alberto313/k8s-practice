622d867dd3dbbc64f8ed776a37fbbfadcbb614e0
albertorv <alberto_rodriguez@epam.com>
2022-12-21 09:33:43 -0600

    initial commit

---
+++ src/test/java/com/alberto/k8spractice/K8sPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberto.k8spractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
